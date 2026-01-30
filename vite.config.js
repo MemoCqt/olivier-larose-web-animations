dd35e98ff75950adffe29889483c6ac93a0f5f87
Guillaume Carquet <guillaume.carquet@infotel.com>
2026-01-30 22:44:35 +0100

    chore(app): force full page refresh on every change

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// force full reload of the page on Ctrl+S
+const fullReloadAlways = () => ({
+  name: 'full-reload-always',
+  handleHotUpdate({ server }) {
+    server.ws.send({type: "full-reload"})
+    return []
+  }
+});
+
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    fullReloadAlways(),
     react()
   ]
-})
+});
