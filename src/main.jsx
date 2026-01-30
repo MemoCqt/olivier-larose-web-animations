657c967d6a6607bed6577190c648631b9c166abc
Guillaume Carquet <guillaume.carquet@infotel.com>
2026-01-30 22:54:47 +0100

    chore(navigation): install react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
+import AppRoutes from "./routes/AppRoutes.jsx";
 import "./index.css";
-import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(<AppRoutes />);
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "../App.jsx";
+import Home from "../pages/Home.jsx";
+import NotFound from "../pages/NotFound.jsx";
+
+const routingTable = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />, // always displayed
+    errorElement: <NotFound />,
+    children: [
+        { path: "", element: <Home /> }
+    ]
+  }
+]);
+
+const AppRoutes = () => {
+    return (<RouterProvider router={routingTable}/>)
+};
+
+export default AppRoutes;
