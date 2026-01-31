48672e1ce56fc71de4139f225245ee38f3de8912
Guillaume Carquet <guillaume.carquet@infotel.com>
2026-01-31 22:48:25 +0100

    feat(animations): add text parallax

---
+++ src/pages/TextParallax.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useRef } from "react";
+import { useScroll, useTransform, motion } from "framer-motion";
+import Picture1 from "../assets/1.jpg";
+import Picture2 from "../assets/2.jpg";
+import Picture3 from "../assets/3.jpg";
+import Lenis from "lenis";
+
+const TextParallax = () => {
+  const container = useRef();
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start end", "end start"]
+  })
+
+  useEffect( () => {
+    const lenis = new Lenis()
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
+  }, [])
+
+  return (
+    <main className="overflow-hidden">
+      <div className="h-screen"/>
+      <div ref={container}>
+        <Slide src={Picture1} direction={"left"} left={"-40%"} progress={scrollYProgress} />
+        <Slide src={Picture2} direction={"right"} left={"-25%"} progress={scrollYProgress} />
+        <Slide src={Picture3} direction={"left"}  left={"-75%"} progress={scrollYProgress} />
+      </div>
+      <div className="h-screen" />
+    </main>
+  );
+};
+
+const Slide = (props) => {
+  const direction = props.direction == "left" ? -1 : 1;
+  const translateX = useTransform(props.progress, [0, 1], [150 * direction, -150 * direction])
+  return (
+    <motion.div style={{x: translateX, left: props.left}} className="relative flex whitespace-nowrap">
+      <Phrase src={props.src}/>
+      <Phrase src={props.src}/>
+      <Phrase src={props.src}/>
+    </motion.div>
+  )
+};
+
+const Phrase = ({src}) => {
+  return (
+    <div className={'px-5 flex gap-5 items-center'}>
+      <p className='text-[7.5vw]'>Front End Developer</p>
+      <span className="relative h-[7.5vw] aspect-4/2 rounded-full overflow-hidden">
+        <img style={{objectFit: "cover"}} src={src} alt="image" fill="true" />
+      </span>
+    </div>
+  )
+};
+
+export default TextParallax;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App.jsx";
 import Home from "../pages/Home.jsx";
+import TextParallax from "../pages/TextParallax.jsx";
 import NotFound from "../pages/NotFound.jsx";
 
 const routingTable = createBrowserRouter([
@@ -9,7 +10,8 @@ const routingTable = createBrowserRouter([
     element: <App />, // always displayed
     errorElement: <NotFound />,
     children: [
-        { path: "", element: <Home /> }
+        { path: "", element: <Home /> },
+        { path: "/text-parallax", element: <TextParallax /> }
     ]
   }
 ]);
